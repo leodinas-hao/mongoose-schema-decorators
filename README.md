2db1c6a677d10a17a67121906a6bc730560c0403
Leodinas Hao <leodinas.hao@gmail.com>
2017-05-31 14:02:36 +1000

    added sample.ts as usage sample

--- README.md
+++ README.md
@@ -18,7 +18,57 @@ Define your mongoose models/schemas easily with Typescript decorators
 * **@virtuals(options?: any)** - decorates a getter/setter/property (virtual reference to build relationship between collections) as schema virtuals
 
 ## Sample usage
-TBA
+```
+interface Customer {
+  _id: number;
+  name: string;
+  createdDate: Date;
+
+  readonly services: Service[];
+  readonly myGetter: string;
+}
+
+type CustomerDocument = Customer & Mongoose.Document;
+
+@schema({
+  toJSON: {
+    getters: true,
+    virtuals: true
+  }
+})
+class CustomerClass extends Mongoose.Model implements Customer {
+  @indexed
+  @unique
+  _id: number;
+
+  @field
+  @indexed
+  @unique
+  name: string;
+
+  @field(Date)
+  createdDate: Date;
+
+  @virtuals({
+    ref: 'Service',
+    localField: '_id',
+    foreignField: 'customer',
+    justOne: false
+  })
+  services: Service[];
+
+  static Search(term: string): Promise<CustomerDocument[]> {
+    let reg = new RegExp(term, 'i');
+    return this.find({ name: reg }).exec();
+  }
+
+  get myGetter(): string {
+    return 'sample getter';
+  }
+}
+
+const CustomerSchema: Mongoose.Schema = buildSchema(CustomerClass);
+```
 
 ## License
 MIT
---
+++ src/sample.ts
@@ -0,0 +1,83 @@
+import * as Mongoose from 'mongoose';
+
+import {
+  buildSchema,
+  field, required, indexed, unique,
+  statics, virtuals, methods,
+  schema
+} from './';
+
+interface Service {
+  _id: Mongoose.Schema.Types.ObjectId;
+  product: string;
+  description: string;
+  customer: number;
+}
+
+type ServiceDocument = Service & Mongoose.Document;
+
+class ServiceClass extends Mongoose.Model implements Service {
+  @field(Mongoose.Schema.Types.ObjectId)
+  _id: Mongoose.Schema.Types.ObjectId
+
+  @field
+  product: string;
+
+  @field
+  description: string;
+
+  @field({ ref: 'Customer' })
+  customer: number;
+}
+
+const ServiceSchema: Mongoose.Schema = buildSchema(ServiceClass);
+
+interface Customer {
+  _id: number;
+  name: string;
+  createdDate: Date;
+
+  readonly services: Service[];
+  readonly myGetter: string;
+}
+
+type CustomerDocument = Customer & Mongoose.Document;
+
+@schema({
+  toJSON: {
+    getters: true,
+    virtuals: true
+  }
+})
+class CustomerClass extends Mongoose.Model implements Customer {
+  @indexed
+  @unique
+  _id: number;
+
+  @field
+  @indexed
+  @unique
+  name: string;
+
+  @field(Date)
+  createdDate: Date;
+
+  @virtuals({
+    ref: 'Service',
+    localField: '_id',
+    foreignField: 'customer',
+    justOne: false
+  })
+  services: Service[];
+
+  static Search(term: string): Promise<CustomerDocument[]> {
+    let reg = new RegExp(term, 'i');
+    return this.find({ name: reg }).exec();
+  }
+
+  get myGetter(): string {
+    return 'sample getter';
+  }
+}
+
+const CustomerSchema: Mongoose.Schema = buildSchema(CustomerClass);
